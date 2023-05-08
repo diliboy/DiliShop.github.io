4823ce5bb198c0a6d2706db0e8b3bf4457bf8be8
ideabiz_user <user.bucket@ideabiz.lk>
2023-05-09 01:32:51 +0530

    deploy website-with home-2-timer changed

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -12,7 +12,7 @@ const Clock = () => {
     let interval;
 
     const countDown =()=>{
-        const destination = new Date('Mar 05, 2023').getTime()
+        const destination = new Date('Jun 05, 2023').getTime()
         interval = setInterval(()=>{
             const now = new Date().getTime()
             const different = destination - now
